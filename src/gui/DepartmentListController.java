4dc57ec3e43cfdc9c19dbed9d89c8c3befd2781e
Alexandre Araujo <alexandre_araujo1704@outlook.com>
2020-05-13 12:18:24 -0300

    Seller TableView

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -73,7 +73,7 @@ public class DepartmentListController implements Initializable, DataChangeListen
 	}
 
 	private void initializeNodes() {
-		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("Id"));
+		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
 
 		Stage stage = (Stage) Main.getMainScene().getWindow();
